d28b2427b186b4dc8646415e6d8e76011c0f615e
Anders Törnqvist <anders@elabs.se>
2011-06-10 10:19:17 -0700

    Added AssertHash to Resto namespace

--- lib/resto/extra/assert_hash.rb
+++ lib/resto/extra/assert_hash.rb
@@ -1,20 +1,22 @@
 # encoding: utf-8
 
-class AssertHash
+module Resto
+  class AssertHash
 
-  def self.keys(hash, *valid_keys)
-    hash ||= {}
+    def self.keys(hash, *valid_keys)
+      hash ||= {}
 
-    hash.each { |key, value| hash[key.to_sym] = hash.delete(key) }
+      hash.each { |key, value| hash[key.to_sym] = hash.delete(key) }
 
-    known_keys = [valid_keys].flatten
-    unknown_keys = hash.keys - known_keys
-    unless unknown_keys.empty?
-      unknown = "Invalid key(s): #{unknown_keys.join(", ")}"
-      known = "Valid key(s): #{known_keys.join(", ")}"
-      raise(ArgumentError, "#{unknown} \n #{known}")
+      known_keys = [valid_keys].flatten
+      unknown_keys = hash.keys - known_keys
+      unless unknown_keys.empty?
+        unknown = "Invalid key(s): #{unknown_keys.join(", ")}"
+        known = "Valid key(s): #{known_keys.join(", ")}"
+        raise(ArgumentError, "#{unknown} \n #{known}")
+      end
+
+      hash
     end
-
-    hash
   end
 end
